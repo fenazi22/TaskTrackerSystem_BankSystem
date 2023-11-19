1f34c44a5f232ee8fe5183b3bf26bc81018cc226
Faisal Abdullah Alanazi <fenazi22@gmail.com>
2023-11-19 10:50:53 +0300

    Update BankController.java
    update

--- BankManagementSystem/src/main/java/com/example/bankmanagementsystem/BankController/BankController.java
+++ BankManagementSystem/src/main/java/com/example/bankmanagementsystem/BankController/BankController.java
@@ -42,17 +42,17 @@ public class BankController {
 
 
     @PutMapping("/Withdraw/{index}")
-    public ApiResponse Withdraws(@PathVariable int index) {
-        BankManagementSystemModel withdrawing = bankModels.get(index);
-        if (withdrawing.getWithdraw() > withdrawing.getBalance()) {
+    public ApiResponse Withdraws(@PathVariable int index,@RequestBody BankManagementSystemModel withdraws) {
+        if (withdraws.getBalance() < withdraws.getWithdraw()) {
             return new ApiResponse("You cannot withdraw because  withdraw more than  your balance", "404");
         } else {
-            withdrawing.setBalance(withdrawing.getBalance() - withdrawing.getWithdraw());
-            bankModels.set(index, withdrawing);
+            withdraws.setBalance(withdraws.getBalance() - withdraws.getWithdraw());
+            bankModels.set(index, withdraws);
             return new ApiResponse("Seccefully Withdraw", "200");
         }
 
     }
+
 
 //    @PutMapping("/deposit/{index}")
 //
@@ -66,14 +66,15 @@ public class BankController {
 
 
     @PutMapping("/deposit/{index}")
-    public ApiResponse deposit(@PathVariable int index) {
-        BankManagementSystemModel deposit = bankModels.get(index);
-        for (BankManagementSystemModel search : bankModels) {
-            if(search.getDeposit()<search.getBalance())
-            search.setBalance(search.getBalance() + search.getDeposit());
-            bankModels.set(index, search);
+    public ApiResponse deposit(@PathVariable int index,@RequestBody BankManagementSystemModel deposits) {
+        if (deposits.getDeposit() >0){
+            deposits.setBalance(deposits.getBalance() + deposits.getDeposit());
+            bankModels.set(index, deposits);
             return new ApiResponse("Successfully deposited", "200");
+
+    }else{
+            return  new ApiResponse("filed ","404");
         }
-           return  new ApiResponse("filed ","404");
+
         }
     }
